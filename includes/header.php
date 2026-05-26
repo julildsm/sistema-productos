50caf3695a69455155a605c3d882b318e08954d5
juli <julianaluz.ledesma@icloud.com>
2026-05-26 16:29:44 -0300

    Inicio del proyecto

---
+++ includes/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Proyecto</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+
+<nav class="navbar navbar-dark bg-dark">
+    <div class="container-fluid">
+        <span class="navbar-brand">Sistema de Productos</span>
+    </div>
+</nav>
+
+<div class="container mt-4">
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+include 'includes/header.php';
+?>
+
+<h1>holaaaaaa</h1>
+
+<?php
+include 'includes/footer.php';
+?>
