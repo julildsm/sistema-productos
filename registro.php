e62c391f8a1d3447fefaccf9a50770b441798c96
juli <julianaluz.ledesma@icloud.com>
2026-05-27 15:43:26 -0300

    Agregue formulario de registro

---
+++ registro.php
@@ -0,0 +1,30 @@
+<?php
+include 'includes/header.php';
+?>
+<h2>Registro de Usuario </h2>
+<form>
+    <div class="mb-3">
+        <label class="form-label">Nombre</label>
+        <input type="text" class="form-control">
+    </div>
+
+    <div class="mb-3">
+        <label class="form-label">Email</label>
+        <input type="email" class="form-control">
+    </div>
+
+    <div class="mb-3">
+        <label class="form-label">Contraseña</label>
+        <input type="password" class="form-control">
+    </div>
+
+    <button class="btn btn-primary">
+        Registrarse
+    </button>
+
+</form>
+
+<?php
+include 'includes/footer.php';
+?>
+</div>
