e8485ffba4142add2f9ca764f7a266a3801d486a
juli <julianaluz.ledesma@icloud.com>
2026-05-27 16:30:53 -0300

    Modificnado usuario.php

--- clases/usuario.php
+++ clases/usuario.php
@@ -8,29 +8,24 @@ class usuario{
         $this->conn = $db;
     }
 
-    public function registrar ($nombre, $email, $password){
-        $passwordhash = password_hash($password, PASSWORD_DEFAULT);
+   public function registrar($nombre, $email, $password) {
 
-        $sql = "insert into" . $this->tabla . " (nombre, email, password) VALUES (:nombre, :email, :password)";
+        $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+
+        $sql = "INSERT INTO " . $this->tabla . "
+                (nombre, email, password)
+                VALUES
+                (:nombre, :email, :password)";
 
         $stmt = $this->conn->prepare($sql);
-        $stmt->bindparam(":nombre", $nombre);
-        $stmt->bindparam(":email", $email);
-        $stmt->bindparam(":password", $password);
-        
-        $stmt->execute();
 
-        $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
+        $stmt->bindParam(":nombre", $nombre);
+        $stmt->bindParam(":email", $email);
+        $stmt->bindParam(":password", $passwordHash);
 
-        if ($usuario && password_verify($password, $usuario['password'])) {
-            $_SESSION['usuario'] = $usuario['nombre'];
+        return $stmt->execute();
 
-            return true;
-        }
-        return false;
     }
 }
-
-
 
 ?>
