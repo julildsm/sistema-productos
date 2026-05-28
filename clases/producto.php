ca0f69932cfb3158a745cbbc44281e67c872637f
mia-estevez <miaolivera88@gmail.com>
2026-05-28 14:19:07 -0300

    Agregando funciones a productos.php

---
+++ clases/producto.php
@@ -0,0 +1,37 @@
+<?php
+
+class producto {
+    private $conn;
+    private $tabla = "productos";
+
+    public function __construct($db){
+        $this->conn = $db;
+    }
+
+    public function crear ($nombre, $descripcion, $precio, $stock){
+        $sql = "INSER TO productos 
+            (nombre, descripcion, precio, stock)
+            VALUES
+            (:nombre, :descripcion, :precio, :stock)";
+
+        $stmt = $this->conn->prepare($sql);
+
+        $stmt->bindParam(":nombre", nombre);
+        $stmt->bindParam(":descripcion", descripcion);
+        $stmt->bindParam(":precio", precio);
+        $stmt->bindParam(":stock", stock);
+
+        return $stmt->execute();
+    }
+
+    public function listar(){
+        $sql = "SELECT * FROM productos";
+
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute();
+
+        return $stmt;
+    }
+}
+
+?>
