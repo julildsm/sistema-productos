07c6b14fbf4960c2d71180b4acd34af64dc1068d
juli <julianaluz.ledesma@icloud.com>
2026-05-28 14:17:49 -0300

    Modificando login.php

--- login.php
+++ login.php
@@ -9,7 +9,7 @@ $conn = $db->conectar();
 
 $usuario = new usuario($conn);
 
-if ($_POST) {
+if (isset($_POST['email']) && isset($_POST['password'])) {
     $login = $usuario->login(
         $_POST['email'],
         $_POST['password']
@@ -23,10 +23,13 @@ if ($_POST) {
     }
 }
 ?>
-
+<h2>Iniciar Sesión</h2>
 <form method="POST">
-    <input type="email" name="email>
-    <input type="password" name= "password">
+    <label>Email</label>
+    <input type="email" name="email"><br></br>
+
+    <label>Contraseña</label>
+    <input type="password" name= "password"><br></br>
     <button type="submit"> Ingresar </button>
 
 </form>
